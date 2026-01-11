26f9abd2bc6aab1db8e808f9b84b5a066bc74bed
Abubakar Jesse <abubakarjesse08032@gmail.com>
2026-01-12 00:17:31 +0100

    Add comments

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let count = 0;
 
 // Get HTML elements
+// By Ids
 const countLabel = document.getElementById("countLabel");
 const increaseBtn = document.getElementById("increaseBtn");
 const resetBtn = document.getElementById("resetBtn");
